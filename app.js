82730b064d13d007066ce95b373af7bf58be40fb
“GalleryGirl” <audreysmitchell@gmail.com>
2019-10-20 16:06:18 -0500

    Add forgiveness show data. Add gratitudeData.jas and poemData.js. Set up id in app.js

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
 const { ApolloServer, gql } = require('apollo-server');
 const forgivenessData = require('./forgivenessData');
+const gratitudeData = require ('./gratitudeData');
+const poemData = require ('./poemData');
 
 //The Graphql Schema
 const typeDefs = gql`
   type Query {
       forgivenesses: [Forgiveness]
+      forgiveness(_id: ID!): Forgiveness
+      gratitudes: [Gratitude]
+      poems: [Poem]
   }
   type Forgiveness {
       _id: ID!
       title: String!
       content: String
   }
+  type Gratitude {
+      _id: ID!
+      title: String
+      content: String
+  }
+  type Poem {
+    _id: ID!
+    title: String
+    content: String
+}
   `;
 
   //A map of functions which return data for the schema.
   const resolvers = {
       Query: {
-          forgivenesses: () => forgivenessData,
+          forgivenesses: (dontCare,args) => forgivenessData,
+          forgiveness: (_,args) => forgivenessData.find(
+            (forgivenessValue) => (forgivenessValue._id === args._id)
+          ),
+          gratitudes: () => gratitudeData,
+          poems: () => [],
           
       },
   };
